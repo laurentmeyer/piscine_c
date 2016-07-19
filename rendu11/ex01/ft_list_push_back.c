d390d36d65f6487d1a2771750411c2e769366f69
Laurent MEYER <lmeyer@student.42.fr>
2016-07-19 13:05:57 +0200

    ex01 ok

--- rendu11/ex01/ft_list_push_back.c
+++ rendu11/ex01/ft_list_push_back.c
@@ -1,26 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_create_elem.c                                   :+:      :+:    :+:   */
+/*   ft_list_push_back.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lmeyer <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/07/19 09:56:22 by lmeyer            #+#    #+#             */
-/*   Updated: 2016/07/19 12:30:15 by lmeyer           ###   ########.fr       */
+/*   Created: 2016/07/19 12:23:31 by lmeyer            #+#    #+#             */
+/*   Updated: 2016/07/19 13:05:08 by lmeyer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
 
-t_list 	*ft_create_elem(void *data)
+void	ft_list_push_back(t_list **begin_list, void *data)
 {
-	t_list	*list;
+	t_list	*new_elem;
+	t_list	*end_list;
 
-	list = malloc(sizeof(t_list));
-	if (list)
+	new_elem = ft_create_elem(data);
+	if (new_elem)
 	{
-		list->data = data;
-		list->next = NULL;
+		end_list = *begin_list;
+		while (end_list->next != NULL)
+			end_list = end_list->next;	
+		end_list->next = new_elem;
 	}
-	return(list);
 }
